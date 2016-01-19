5f0f47540fdb2fe22ba627919642454b4ed0aa1c
JPMartha <sathachie@gmail.com>
2016-01-20 06:26:21 +0900

    Initial Commit

---
+++ Pancake/Pancake.h
@@ -0,0 +1,19 @@
+//
+//  Pancake.h
+//  Pancake
+//
+//  Created by JPMartha on 2016/01/20.
+//  Copyright © 2016年 JPMartha. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Pancake.
+FOUNDATION_EXPORT double PancakeVersionNumber;
+
+//! Project version string for Pancake.
+FOUNDATION_EXPORT const unsigned char PancakeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Pancake/PublicHeader.h>
+
+
